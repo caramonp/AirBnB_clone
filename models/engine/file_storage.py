53d38702414d5d6156b5e0af35e8fb97e0267c00
Carolina <carolinaramon239578@correo.itm.edu.co>
2021-06-28 18:46:51 -0500

    console and file storage without pep8

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -10,26 +10,47 @@ from uuid import uuid4
 from datetime import datetime
 import json
 import models
+import os
 
 class FileStorage:
+
     __file_path = "file.json"
     __objects = {}
-
+    dict_clases = {"BaseModel": BaseModel}
+    
     def all(self):
-        return self.__objects
+      """Public instance
+
+      Returns:
+          [self]: [the dictionary objects]
+      """
+      return self.__objects
 
     def new(self, obj):
-        key = obj.__class__.__name__ + "." + obj.id
-        self.__objects[key] = obj
+        """[sets in __objects the obj with key <obj class name>.id]
+
+        Args:
+            obj ([objects]): [new objets]
+        """
+        key_compoust = obj.__class__.__name__ + "." + obj.id
+        self.__objects.update({key_compoust: obj})
 
     def save(self):
-        #__file_path = JSON.dump(__objects)
-        #return to_json
-        pass
+        """[serializes __objects to the JSON file]
+        """
+        dict_string = {}
+        with open(self.__file_path, 'w', encoding='UTF8') as file:
+          for key, value in self.__objects.items():
+              dict_string[key] = value.to_dict()
+          file.write(json.dumps(dict_string))
 
     def reload(self):
-        #try:
-        #   my_json_object = json.dump(__objects)
-        # except FileNotFoundError:
-            pass
-
+        """[ deserializes the JSON file to __objects]
+        """
+        try:
+            with open(self.__file_path) as file:
+                dict_desco = json.load(file)
+            for key, value in dict_desco.items():
+                self.__objects[key] = FileStorage.dict_clases[value["__class__"]](**value)
+        except:
+            return
