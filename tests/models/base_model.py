ac34385d16f502ebbe7845ac5edee5b491116d2e
Dania Puertas <daniajibellis@hotmail.com>
2021-06-29 16:05:30 +0000

    correcting errors by pep8

--- tests/models/base_model.py
+++ tests/models/base_model.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """
-class BaseModel that defines all common 
+class BaseModel that defines all common
 methods for other classes
 """
 
@@ -17,39 +17,37 @@ class BaseModel:
     """
     def __init__(self, *args, **kwargs):
         if kwargs:
-          for  key, value in kwargs.items():
-              if key != "__class__":
-                if key == "created_at" or key == "updated_at":
-                        value = datetime.strptime(value, "%Y-%m-%dT%H:%M:%S.%f")
-                setatrr(self, key, value)
+            for key, value in kwargs.items():
+                if key != "__class__":
+                    if key == "created_at" or key == "updated_at":
+                        value = datetime.strptime(value,
+                                                  "%Y-%m-%dT%H:%M:%S.%f")
+                    setatrr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
             self.updated_at = self.created_at
-              
-                  
 
     def __str__(self):
-      """public instance to print name, id, dict
-      """
-        print ("[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__))
-        
-    def save(self):
-      """
-      updates the public instance attribute with the current datetime
-      """
-      self.updated_at = datetime.now()
-        
-        
-        
-    def __dict__(self):
-      """dictionary with the values
+        """public instance to print name, id, dict
+        """
+        print("[{}] ({}) {}".format(self.__class__.__name__,
+                                    self.id, self.__dict__))
 
-      Returns:
-          [dict]: [containing all keys/values ]
-      """
-      dictionary = self.__dict__
-      dictionary["__class__"] = self.__class__.__name__
-      dictionary["created_at"] = self.created_at.isoformat()
-      dictionary["updated_at"] = self.updated_at.isoformat()
-      return dictionary
+    def save(self):
+        """
+        updates the public instance attribute with the current datetime
+        """
+        self.updated_at = datetime.now()
+
+    def __dict__(self):
+        """dictionary with the values
+
+        Returns:
+            [dict]: [containing all keys/values ]
+        """
+        dictionary = self.__dict__
+        dictionary["__class__"] = self.__class__.__name__
+        dictionary["created_at"] = self.created_at.isoformat()
+        dictionary["updated_at"] = self.updated_at.isoformat()
+        return dictionary
