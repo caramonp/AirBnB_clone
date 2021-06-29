bb06e0064e320dcfa2871f92434c959bfe7a2973
vagava <vagava.123@gmail.com>
2021-06-29 06:57:29 -0500

    add ident=4 in save() method

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -17,14 +17,14 @@ class FileStorage:
     __file_path = "file.json"
     __objects = {}
     dict_clases = {"BaseModel": BaseModel}
-    
-    def all(self):
-      """Public instance
 
-      Returns:
-          [self]: [the dictionary objects]
-      """
-      return self.__objects
+    def all(self):
+        """Public instance
+
+        Returns:
+            [self]: [the dictionary objects]
+        """
+        return self.__objects
 
     def new(self, obj):
         """[sets in __objects the obj with key <obj class name>.id]
@@ -40,9 +40,9 @@ class FileStorage:
         """
         dict_string = {}
         with open(self.__file_path, 'w', encoding='UTF8') as file:
-          for key, value in self.__objects.items():
-              dict_string[key] = value.to_dict()
-          file.write(json.dumps(dict_string))
+            for key, value in self.__objects.items():
+                dict_string[key] = value.to_dict()
+            file.write(json.dumps(dict_string, indent=4))
 
     def reload(self):
         """[ deserializes the JSON file to __objects]
