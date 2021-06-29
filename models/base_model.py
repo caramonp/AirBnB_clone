e7d0d47d9613e623d84599c9cac0c994a06d8bf4
vagava <vagava.123@gmail.com>
2021-06-29 08:21:40 -0500

    fix identation of setattr in __init__ constructor

--- models/base_model.py
+++ models/base_model.py
@@ -22,7 +22,7 @@ class BaseModel:
                     if key == "created_at" or key == "updated_at":
                         value = datetime.strptime(value,
                                                   "%Y-%m-%dT%H:%M:%S.%f")
-            setattr(self, key, value)
+                    setattr(self, key, value)
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
