0f0cf7eb6d7f428be5ff308530fe5bf78a5a2339
vagava <vagava.123@gmail.com>
2021-06-30 22:09:59 -0500

    try to fix the check of task 5

--- models/base_model.py
+++ models/base_model.py
@@ -28,7 +28,7 @@ class BaseModel:
         else:
             self.id = str(uuid4())
             self.created_at = datetime.now()
-            self.updated_at = self.created_at
+            self.updated_at = datetime.now()
             models.storage.new(self)
 
     def __str__(self):
@@ -44,6 +44,7 @@ class BaseModel:
         """
         self.updated_at = datetime.now()
         models.storage.save()
+
 
     def to_dict(self):
         """dictionary with the values
